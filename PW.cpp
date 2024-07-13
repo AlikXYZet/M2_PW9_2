c9bd3dd0938c50caf3f0d12f88fe97c781bf7ed7
Александр Осипов <alikxyzet@gmail.com>
2024-07-13 04:44:13 +0300

    9.2 New "GameInput", Adding "GraphicsRendering" (A new thread is required)

--- PW.cpp
+++ PW.cpp
@@ -8,6 +8,8 @@
 #include <thread>   // Управление потоками
 #include <mutex>    // Очерёдность потоков
 
+#include <windows.h>// Для скрытия курсора и обработки нажатий
+
 
 
 /* ---   Потоковые функции   --- */
@@ -16,69 +18,173 @@
 void tf_GameInput(int8_t& out_Command)
 {
     int curInput = 0;
-    int8_t preCommand = 0;
+
+    HANDLE s_in = GetStdHandle(STD_INPUT_HANDLE);
+    DWORD dr;
+    INPUT_RECORD rec;
 
     while (true)
     {
-        curInput = _getch();
+        ReadConsoleInput(s_in, &rec, 1, &dr);
 
-        // Если вводимая клавиша с двойным кодом
-        if (curInput == 0 || curInput == 224)
-            curInput = _getch();    // то взять второй код
+        if (rec.EventType == KEY_EVENT)
+            if (rec.Event.KeyEvent.bKeyDown)
+            {
+                switch (rec.Event.KeyEvent.wVirtualKeyCode)
+                {
+                    // Клавиши "прыжок":
+                case VK_UP:     // Вверх
+                case VK_NUMPAD8:// Num 8
+                case 0x57:      // W
+                    out_Command = 1;
+                    break;
 
-        switch (curInput)
-        {
-            // Клавиши "прыжок":
-        case 32:    // Пробел
-        case 56:    // 8
-        case 72:    // Вверх
-        case 87:    // W
-        case 119:   // w
-        case 150:   // Ц
-        case 230:   // ц
+                    // Клавиши "пригнуться":
+                case VK_DOWN:   // Вниз
+                case VK_NUMPAD2:// Num 2
+                case 0x53:      // S
+                    out_Command = 2;
+                    break;
 
-            if (preCommand == 1)
-                out_Command = 11;
+                    // Клавиши "выход":
+                case VK_ESCAPE:
+                    out_Command = 0;
+                    break;
+                }
+            }
             else
-                out_Command = 1;
+            {
+                switch (out_Command)
+                {
+                case 1:
+                    switch (rec.Event.KeyEvent.wVirtualKeyCode)
+                    {
+                        // Клавиши "прыжок":
+                    case VK_UP:     // Вверх
+                    case VK_NUMPAD8:// Num 8
+                    case 0x57:      // W
+                        out_Command = -1;
+                        break;
+                    }
+                    break;
 
-            break;
-
-            // Клавиши "пригнуться":
-        case 50:    // 2
-        case 80:    // Вниз
-        case 83:    // S
-        case 115:   // s
-        case 155:   // Ы
-        case 235:   // ы
-
-                out_Command = 2;
-
-            break;
-
-            // Клавиши "выход":
-        case 27:    // Esc
-            out_Command = 0;
-            break;
-        }
-
-        // Тест
-        //std::cout << (int)out_Command << '\n';
-
-        preCommand = out_Command;
+                case 2:
+                    switch (rec.Event.KeyEvent.wVirtualKeyCode)
+                    {
+                        // Клавиши "прыжок":
+                    case VK_DOWN:   // Вниз
+                    case VK_NUMPAD2:// Num 2
+                    case 0x53:      // S
+                        out_Command = -1;
+                        break;
+                    }
+                    break;
+                }
+            }
     }
 }
 
 // Рендер графики
-void tf_GraphicsRendering()
+void tf_GraphicsRendering(int8_t& in_Command, uint8_t& out_Position)
 {
+    unsigned int curPoints = 0;
+    uint16_t time = 1000;
+    int upperBorder = 4;
+    int lowerBorder = out_Position = 7;
+    bool bFall = false;
 
+
+
+    std::cout << "Кол. очков: ";
+
+    printf("\x1B[%d;%df", upperBorder - 1, 0);
+
+    std::cout
+        << "------------------------------------------------------------------------------------------------------------------------" << '\n'   // 3
+        << "                                                                                                                        " << '\n'   // 4
+        << "                                                                                                                        " << '\n'   // 5
+        << "                                                                                                                        " << '\n'   // 6
+        << "                                                                                                                        " << '\n'   // 7
+        << "------------------------------------------------------------------------------------------------------------------------" << '\n'   // 8
+        << std::endl;
+
+
+
+    while (in_Command)
+    {
+        // Обработка команд
+        switch (in_Command)
+        {
+        case 1:
+
+            if (out_Position == lowerBorder)
+            {
+                --out_Position;
+                bFall = true;
+            }
+            else if (bFall)
+            {
+                --out_Position;
+                bFall = false;
+            }
+            else
+            {
+                ++out_Position;
+            }
+
+            break;
+        case 2:
+            out_Position = lowerBorder;
+
+            break;
+
+        default:
+            if (out_Position == lowerBorder)
+                bFall = false;
+            else
+                ++out_Position;
+
+            break;
+        }
+
+
+
+        /* ---   Отрисовка   --- */
+
+        // Количество очков:
+        ++curPoints;
+        printf("\x1B[%d;%df", 1, 13);
+        std::cout << curPoints << std::endl;
+
+        // Дино:
+        for (int i = upperBorder; i <= lowerBorder; ++i)
+        {
+            printf("\x1B[%d;%df", i, 3);
+            std::cout << " ";
+        }
+
+        printf("\x1B[%d;%df", int(out_Position), 3);
+        std::cout << "$" << std::endl;
+        //----------------------------------------
+
+
+
+        // Задержка времени: имитация скорости бега
+        if (time > 50)
+            time = 100 - (curPoints);
+        else
+            time = 50;
+        std::this_thread::sleep_for(std::chrono::milliseconds(time));
+    }
 }
 
 // Проверка препятствий
-void tf_CheckingObstacles()
+void tf_CheckingObstacles(const uint8_t& out_Position)
 {
+    while (true)
+    {
 
+    }
 }
 //---------------------------------------------------------------------
 
@@ -93,6 +199,13 @@ int main()
 
     // Использовать текущее время в качестве начального значения для генератора псевдослучайных чисел
     srand(time(0));
+
+    // Скрытие курсора консоли
+    auto handle = GetStdHandle(STD_OUTPUT_HANDLE);
+    CONSOLE_CURSOR_INFO structCursorInfo;
+    GetConsoleCursorInfo(handle, &structCursorInfo);
+    structCursorInfo.bVisible = FALSE;
+    SetConsoleCursorInfo(handle, &structCursorInfo);
     //----------------------------------------
 
 
@@ -101,6 +214,9 @@ int main()
 
     // Текущая комманда
     int8_t curCommand = -1;
+
+    // Текущая позиция
+    uint8_t curPosition = 1;
     //----------------------------------------
 
 
@@ -109,25 +225,27 @@ int main()
 
     // Ввод данных
     std::thread t_GameInput = std::thread(tf_GameInput, std::ref(curCommand));
+    t_GameInput.detach();
 
     // Рендер графики
-    std::thread t_GraphicsRendering = std::thread(tf_GraphicsRendering);
+    std::thread t_GraphicsRendering = std::thread(tf_GraphicsRendering, std::ref(curCommand), std::ref(curPosition));
+    t_GraphicsRendering.detach();
 
     // Проверка препятствий
-    std::thread t_CheckingObstacles = std::thread(tf_CheckingObstacles);
+    std::thread t_CheckingObstacles = std::thread(tf_CheckingObstacles, std::ref(curPosition));
+    t_CheckingObstacles.detach();
     //----------------------------------------
 
 
 
     // Бесконечный цикл ожидания "Esc"
     while (curCommand != 0) {}
+    curPosition = 0;
 
 
 
-    // Принудительное закрытие потоков
-    t_GameInput.detach();
-    t_GraphicsRendering.detach();
-    t_CheckingObstacles.detach();
+    printf("\x1B[%d;%df", 10, 0);
+    system("pause");
 
     return 0;
 }
